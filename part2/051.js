507588bf19dee49d51bebac5f83da42642e3e878
jinseong205 <jinseong205@gmail.com>
2021-09-28 23:32:48 +0900

    Create 051.js

---
+++ part2/051.js
@@ -0,0 +1,21 @@
+class Cart { 
+    constructor(){
+        this.store = {};
+    }
+
+    addProduct(product){
+        this.store[product.id] = product;
+    }
+
+    getProudct(id){
+        return this.store[id];
+    }
+}
+
+const c = new Cart();
+
+c.addProduct({id: 1, name : '노트북'});
+console.log(c.store);
+
+const p = c.getProudct(1);
+console.log(p);
